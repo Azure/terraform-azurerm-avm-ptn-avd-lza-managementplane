a172dc9166f607f59c3690d488c420b88ee3a764
Jen Sheerin <jensheerin@microsoft.com>
2024-02-27 23:37:31 -0500

    outputs update

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
+# Module owners should include the full resource via a 'resource' output
+# https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
+output "resource" {
+  value       = azurerm_resource_group.this
+  description = "This is the full output for the resource."
+}
+
+output "private_endpoints" {
+  value       = azurerm_private_endpoint.this
+  description = "A map of private endpoints. The map key is the supplied input to var.private_endpoints. The map value is the entire azurerm_private_endpoint resource."
+}
 
 output "azure_virtual_desktop_host_pool" {
   description = "Name of the Azure Virtual Desktop host pool"
@@ -27,18 +38,4 @@ output "azurerm_virtual_desktop_workspace" {
 output "workspace_id" {
   value       = azurerm_virtual_desktop_workspace.workspace.id
   description = "The ID of the Workspace resource."
-
-# TODO: insert outputs here.
-
-# Module owners should include the full resource via a 'resource' output
-# https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
-output "resource" {
-  value       = azurerm_resource_group.TODO # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
-  description = "This is the full output for the resource."
-
-}
-
-output "private_endpoints" {
-  value       = azurerm_private_endpoint.this
-  description = "A map of private endpoints. The map key is the supplied input to var.private_endpoints. The map value is the entire azurerm_private_endpoint resource."
 }
