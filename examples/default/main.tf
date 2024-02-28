6d1078e1102483aba20a9f5b3cb7fc7267ce6efa
Jen Sheerin <jensheerin@microsoft.com>
2024-02-27 23:40:57 -0500

    resolve conflicts

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -68,20 +68,4 @@ module "avd" {
       workspace_resource_id = azurerm_log_analytics_workspace.this.id
     }
   }
-=======
-  location = module.regions.regions[random_integer.region_index.result].name
-}
-
-# This is the module call
-# Do not specify location here due to the randomization above.
-# Leaving location as `null` will cause the module to use the resource group location
-# with a data source.
-module "test" {
-  source = "../../"
-  # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
-  # ...
-  enable_telemetry    = var.enable_telemetry # see variables.tf
-  name                = "TODO"               # TODO update with module.naming.<RESOURCE_TYPE>.name_unique
-  resource_group_name = azurerm_resource_group.this.name
-
 }
