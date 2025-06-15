38f5f969a5ec54f017fe42cb4d904c5f11d2d7b9
Jen Sheerin <jensheerin@microsoft.com>
2025-06-15 17:42:19 -0400

    feat: add azapi provider to terraform configuration

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 1.9, < 2.0"
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">= 1.0.0"
+    }
     azuread = {
       source  = "hashicorp/azuread"
       version = ">= 2.0.0, < 3.0.0"
